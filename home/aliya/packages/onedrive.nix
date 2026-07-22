85af9ba6075f951fe6d3eb2bc4c867db7fc9aefe
Aliya Gamez <contact@aliyagamez.com>
2026-07-21 23:37:55 -0400

    onedrive settings

--- home/aliya/packages/onedrive.nix
+++ home/aliya/packages/onedrive.nix
@@ -3,5 +3,18 @@
 {
   programs.onedrive = {
     enable = true;
+
+    settings = {
+      sync_dir = "/home/aliya/onedrive";
+
+      monitor_interval = "300";
+      skip_dotfiles = "true";
+      check_nomount = "false";
+      check_nosync = "false";
+
+      classify_as_big_delete = "1000";
+      disable_notifications = "false";
+      rate_limit = "0";
+    };
   };
 }
