1f936c3485090c26e1f324e918e9d93da84aa928
Aliya Gamez <contact@aliyagamez.com>
2026-07-22 01:01:33 -0400

    gnome syntax fix

--- home/aliya/desktop/gnome.nix
+++ home/aliya/desktop/gnome.nix
@@ -77,27 +77,28 @@
             "dash-to-dock@micxgx.gmail.com"
           ];
       };
-	  # Extension: Dash to Dock
-	  "org/gnome/shell/extensions/dash-to-dock" = {
-		dock-fixed = true;
-		dash-max-icon-size = 32;
-		show-trash = false;
-		click-action = "minimize-or-overview";
 
-		transparency-mode = "DYNAMIC";
-		customize-alphas = "true";
-		min-alpha = 0.35;
-		max-alpha = 0.70;
-	  };
-	  # Extension: Vitals
-		"/org/gnome/shell/extensions/vitals" = {
-		alphabetize = false;
-		use-higher-precision = true;
-		fixed-widths = false;
-		update-time = 3;
-		hot-sensors = ['_memory_allocated_', '_memory_usage_', '_processor_usage_', '__network-rx_max__', '__network-tx_max__', '_voltage_bat0_in0_'];
-		
-		};
+      # Extension: Dash to Dock
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        dock-fixed = true;
+        dash-max-icon-size = 32;
+        show-trash = false;
+        click-action = "minimize-or-overview";
+
+        transparency-mode = "DYNAMIC";
+        customize-alphas = "true";
+        min-alpha = 0.35;
+        max-alpha = 0.70;
+      };
+      
+      # Extension: Vitals
+      "/org/gnome/shell/extensions/vitals" = {
+        alphabetize = false;
+        use-higher-precision = true;
+        fixed-widths = false;
+        update-time = 3;
+        hot-sensors = ['_memory_allocated_', '_memory_usage_', '_processor_usage_', '__network-rx_max__', '__network-tx_max__', '_voltage_bat0_in0_'];
+      };
 
     };
   };
