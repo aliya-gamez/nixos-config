d81f911b94e82ec3655bb6e8b171615da239124e
Aliya Gamez <contact@aliyagamez.com>
2026-07-22 10:47:31 -0400

    terminal

--- home/aliya/packages/terminal.nix
+++ home/aliya/packages/terminal.nix
@@ -4,10 +4,18 @@
   # Terminal: Ghostty
   programs.ghostty = {
     enable = true;
+
+    settings = {
+      background-opacity = 0.90;
+      window-padding-x = "4,4";
+
+      shell-integration = "none";
+    };
   };
 
   # Terminal-based packages
   home.packages = with pkgs; [
     fastfetch
+    btop
   ];
 }
