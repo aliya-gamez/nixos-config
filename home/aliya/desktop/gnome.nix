d43bc391a913ecddc87073016955baabe5f21e7b
Aliya Gamez <contact@aliyagamez.com>
2026-07-22 00:30:11 -0400

    gnome settings edits

--- home/aliya/desktop/gnome.nix
+++ home/aliya/desktop/gnome.nix
@@ -6,43 +6,59 @@
 
     settings = {
 
-      # GNOME desktop behavior
+      ### Gnome Desktop
+
+      # Interface
       "org/gnome/desktop/interface" = {
         accent-color = "pink";
         color-scheme = "prefer-dark";
         clock-show-weekday = true;
         enable-hot-corners = false;
-      };
-
-
-      # Disable animations
-      "org/gnome/desktop/interface" = {
         enable-animations = false;
       };
 
+      # Background
+      "org/gnome/desktop/screensaver" = {
+        picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blendpills-l.jxl";
+      };
 
       # Touchpad
       "org/gnome/desktop/peripherals/touchpad" = {
         tap-to-click = true;
         natural-scroll = true;
+        click-method = "areas";
       };
 
+      ### Settings Daemon
 
       # Keyboard shortcuts
       "org/gnome/settings-daemon/plugins/media-keys" = {
         custom-keybindings = [
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/google-chrome/"
         ];
       };
-
 
       # Terminal shortcut
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal" = {
         name = "Terminal";
-        command = "kgx";
+        command = "ghostty";
         binding = "<Super>Return";
       };
 
+      # Google Chrome shortcut
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/google-chrome" = {
+        name = "Google Chrome";
+        command = "google-chrome-stable";
+        binding = "<Super>q";
+      };
+
+      # Nightlight
+      "/org/gnome/settings-daemon/plugins/color" {
+        night-light-enabled = true;
+      }
+
+      ### Nautilus
 
       # Nautilus
       "org/gnome/nautilus/preferences" = {
@@ -50,13 +66,9 @@
         show-hidden-files = true;
       };
 
+      ### Gnome SHell
 
-      # Workspaces
-      "org/gnome/mutter" = {
-        dynamic-workspaces = true;
-      };
-
-      # Extensins
+      # Extensions
       "org/gnome/shell" = {
           enabled-extensions = [
             "Vitals@CoreCoding.com"
