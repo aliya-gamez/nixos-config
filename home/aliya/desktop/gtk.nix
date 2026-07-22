5b09eeedfecee9ba450df4f8c4628714f89b406c
Aliya Gamez <contact@aliyagamez.com>
2026-07-22 16:58:37 -0400

    gtk3

--- home/aliya/desktop/gtk.nix
+++ home/aliya/desktop/gtk.nix
@@ -3,15 +3,16 @@
 {
   gtk = {
     enable = true;
-
-    theme = {
-      name = "catppuccin-mocha-red-standard";
-      package = pkgs.catppuccin-gtk.override {
-        accents = [ "red" ];
-        variant = "mocha";
-        size = "standard";
-      };
+    gtk3 = {
+      enable = true;
+        theme = {
+          name = "catppuccin-mocha-red-standard";
+          package = pkgs.catppuccin-gtk.override {
+            accents = [ "red" ];
+            variant = "mocha";
+            size = "standard";
+          };
+        };
     };
-
   };
 }
