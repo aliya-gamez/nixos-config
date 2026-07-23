ca35a1f0cff3491eaa32f0c418c8dac92e2b66ef
Aliya Gamez <contact@aliyagamez.com>
2026-07-23 14:45:40 -0400

    new desktop config

---
+++ hosts/adt/default.nix
@@ -0,0 +1,98 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ 
+    ./hardware-configuration.nix 
+
+    # Desktop
+    ../../modules/desktop/gnome.nix
+
+    # Programs
+    ../../modules/programs/spotify.nix
+
+    # Services
+    ../../modules/services/docker.nix
+    ../../modules/services/postgresql.nix
+
+    # Themes
+    #../../modules/themes/catppuccin
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "adt"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # User accounts and shell configuration
+  programs.zsh.enable = true;
+  users.users."aliya" = {
+    isNormalUser = true;
+    description = "Aliya Gamez";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh; #shell
+    packages = with pkgs; [];
+  };
+
+  # Reduce Disk Space
+  boot.loader.systemd-boot.configurationLimit = 10;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+  nix.settings.auto-optimise-store = true;
+
+  # Packages
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    # Terminal
+  	git
+  	wget
+    micro
+  ];
+  environment.variables.EDITOR = "micro";
+
+  # Enable VS Code Server
+  programs.nix-ld.enable = true;
+
+  # SSH
+	services.openssh = {
+		enable = true;
+		settings = {
+			X11Forwarding = true;
+			PermitRootLogin = "no";
+			PasswordAuthentication = false;
+		};
+		ports = [ 22 ];
+	};  
+
+  # Time/Locale info
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # X11: Keymap
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # NixOS Version
+  system.stateVersion = "26.05";
+}
